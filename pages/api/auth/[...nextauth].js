fd08a2095d12d7b6c78a0e143096d7dc694da9f4
Joseph Ogbu <fallyfox@gmail.com>
2023-12-05 12:55:15 +0100

    firebase adapter

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth/next';
 import GoogleProvider from 'next-auth/providers/google';
+import { FirestoreAdapter } from "@auth/firebase-adapter";
+import { cert } from "firebase-admin/app";
 
 const authOptions = {
     providers:[
@@ -7,7 +9,14 @@ const authOptions = {
             clientId:process.env.GOOGLE_CLIENT_ID,
             clientSecret:process.env.GOOGLE_CLIENT_SECRET
         }),
-    ]
+    ],
+    adapter: FirestoreAdapter({
+        credential: cert({
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+          privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n") : undefined,
+        })
+    })
 }
 
 export default NextAuth(authOptions);
